fe73868ea37b4bde8132439541b330c9969c8be4
Sergii Proiava <seregikpro@gmail.com>
2022-06-28 01:52:18 +0300

    Stats is styled

--- src/components/Profile/Profile.styled.js
+++ src/components/Profile/Profile.styled.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 export const Card = styled.div`
-  border: 1px solid var(--accent-color);
   border-radius: 10px;
+  background-color: var(--primary-background-color);
+  width: 360px;
+  overflow: hidden;
 
   :hover {
     box-shadow: 0px 3px 2px 2px rgba(100, 100, 150, 0.15);
@@ -46,26 +48,19 @@ export const Location = styled.p`
 
 export const Statistics = styled.ul`
   display: flex;
-  /* flex-wrap: nowrap; */
   justify-content: center;
   align-content: space-between;
-  margin: 0;
-  padding: 0;
   list-style: none;
-  border-top: 1px solid var(--accent-color);
 `;
 
 export const StatsElement = styled.li`
   display: block;
   width: 120px;
-  background-color: rgb(250, 250, 250);
+  background-color: var(--secondary-background-color);
   padding: 25px;
 
-  :not(:last-child) {
-    border-right: 1px solid var(--accent-color);
-  }
   :hover {
-    background-color: #fff;
+    background-color: var(--primary-background-color);
   }
 `;
 
@@ -80,7 +75,7 @@ export const Label = styled.span`
 export const Quantity = styled.span`
   display: block;
   text-align: center;
-  font-size: 18px;
+  font-size: 20px;
   font-weight: 700;
   color: var(--primary-text-color);
   margin-top: 10px;
--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -1,41 +1,53 @@
 import styled from 'styled-components';
 
 export const StatSection = styled.section`
-  margin: 8px;
+  border-radius: 10px;
+  background-color: var(--primary-background-color);
   width: 360px;
+  overflow: hidden;
   text-align: center;
-  color: var(--mid-text-color);
-  background-color: var(--light-back-color);
+
+  :hover {
+    box-shadow: 0px 3px 2px 2px rgba(100, 100, 150, 0.15);
+  }
 `;
+
 export const Title = styled.h2`
-  display: block;
-  margin: 0 auto;
   padding: 25px;
-  font-family: Arial, sans-serif;
-  font-size: 18px;
-  font-weight: 400;
+  font-size: 20px;
+  font-weight: 700;
   text-transform: uppercase;
 `;
+
 export const StatList = styled.ul`
-  list-style: none;
   display: flex;
-  flex-wrap: nowrap;
-  justify-content: baseline;
+  justify-content: center;
   align-content: space-between;
+  list-style: none;
 `;
+
 export const StatItem = styled.li`
   flex-grow: 1;
   display: block;
-  color: var(--light-text-color);
-  background-color: ${props => props.generatedColor};
-  padding: 16px 5px;
-  border-top: 1px var(--mid-back-color) solid;
+  background-color: var(--secondary-background-color);
+  padding: 15px 5px;
+
+  :hover {
+    background-color: var(--primary-background-color);
+  }
 `;
+
 export const Label = styled.span`
   display: block;
-  font-size: 14px;
+  text-align: center;
+  font-size: 16px;
+  color: var(--secondary-text-color);
 `;
+
 export const Percentage = styled.span`
   display: block;
+  text-align: center;
   font-size: 24px;
+  color: var(--primary-text-color);
+  margin-top: 10px;
 `;
